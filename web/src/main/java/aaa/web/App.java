33febf0e3c7c009c201812496623a1eb465f8f5f
Mathai <Mathai@ATHS-DELL>
2021-12-08 16:21:38 -0600

    first commit

---
+++ web/src/main/java/aaa/web/App.java
@@ -0,0 +1,13 @@
+package aaa.web;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
